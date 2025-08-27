b12c884b99b1d512b55554d4f6379b260be5d46d
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-27 16:46:38 +0700

    Add: DbContext in Model/Context & Db file in Database folder

---
+++ WinForm-MVC-Sqlite/Model/Context/DbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SQLite;
+
+namespace WinForm_MVC_Sqlite.Model.Context
+{
+    public class DbContext : IDisposable
+    {
+        private SQLiteConnection _conn;
+
+        public SQLiteConnection Conn
+        {
+            get { return _conn ?? (_conn = GetOpenConnection()); }
+        }
+
+        private SQLiteConnection GetOpenConnection()
+        {
+            SQLiteConnection conn = null;
+
+            try
+            {
+                string dbName = @"D:\WinForm Project\WinForm-MVC-Sqlite\Database\DbMahasiswa.db";
+                string connectionString = string.Format("Data Source={0};FailIfMissing=True", dbName);
+
+                conn = new SQLiteConnection(connectionString);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("Open connection error: {0}", ex.Message);
+            }
+
+            return conn;
+        }
+
+        public void Dispose()
+        {
+            if (_conn != null)
+            {
+                try
+                {
+                    if (_conn.State != ConnectionState.Closed) _conn.Close();
+                }
+                finally
+                {
+                    _conn.Dispose();
+                }
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
+}
