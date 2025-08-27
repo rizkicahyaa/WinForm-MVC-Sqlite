80cc3add208b0a2d171f2641a5185cc819503b66
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-27 18:01:15 +0700

    Add: MahasiswaController in Controller

---
+++ WinForm-MVC-Sqlite/Controller/MahasiswaController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using WinForm_MVC_Sqlite.Model.Context;
+using WinForm_MVC_Sqlite.Model.Entity;
+using WinForm_MVC_Sqlite.Model.Repository;
+
+namespace WinForm_MVC_Sqlite.Controller
+{
+    public class MahasiswaController
+    {
+        private MahasiswaRepository _repository;
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NIM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            
+            if (string.IsNullOrEmpty(mhs.Jurusan))
+            {
+                MessageBox.Show("Jurusan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Create(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NIM harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(mhs.Jurusan))
+            {
+                MessageBox.Show("Jurusan harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Update(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil diupdate !", "Informasi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal diupdate !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NIM harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Delete(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil dihapus !", "Informasi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal dihapus !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            return result;
+        }
+
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                list = _repository.ReadByNama(nama);
+            }
+            return list;
+        }
+
+        public List<Mahasiswa> ReadAll()
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
+    }
+}
